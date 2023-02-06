59306334f0eb816efed87ab1c3513e293cbbdf20
jxch <15553598935@163.com>
2023-02-07 05:17:16 +0800

    兼容没有成交量的数据

--- test/core/test_pdr_core.py
+++ test/core/test_pdr_core.py
@@ -1,9 +1,14 @@
 import core.pdr_core as pdr_core
 import datetime
 import logging
+import pandas_datareader.data as web
 
 logging.basicConfig(level=logging.INFO)
 
-df = pdr_core.data_reader_codes_json(["PG"], datetime.datetime(2022, 1, 1))
+df = pdr_core.data_reader_codes_json(["^CAC"], datetime.datetime(2022, 1, 1))
 
+
+
+
+# df = web.DataReader("^DAX", "stooq", datetime.datetime(2022, 1, 1), datetime.datetime.now())
 print(df)
--- utils/stockstats_utils.py
+++ utils/stockstats_utils.py
@@ -3,11 +3,18 @@ import stockstats
 
 def stockstats_default(old_df):
     ss = stockstats.StockDataFrame.retype(old_df)
-    df = ss[['open', 'close', 'high', 'low', 'volume',
-             'tr', 'atr', 'close_-1_d', 'close_5_sma', 'close_20_sma', 'close_60_sma', 'close_120_sma', 'close_200_sma',
-             'close_20_sma_-1_d', 'close_60_sma_-1_d', 'close_120_sma_-1_d', 'close_5_ema',
-             'close_20_ema', 'close_60_ema', 'close_120_ema', 'close_20_ema_-1_d', 'close_200_ema',
-             'macd', 'macds', 'macdh', 'kdjk', 'kdjd', 'kdjj']]
+    if 'volume' in old_df:
+        df = ss[['open', 'close', 'high', 'low', 'volume',
+                 'tr', 'atr', 'close_-1_d', 'close_5_sma', 'close_20_sma', 'close_60_sma', 'close_120_sma',
+                 'close_200_sma',
+                 'close_20_sma_-1_d', 'close_60_sma_-1_d', 'close_120_sma_-1_d', 'close_5_ema',
+                 'close_20_ema', 'close_60_ema', 'close_120_ema', 'close_20_ema_-1_d', 'close_200_ema',
+                 'macd', 'macds', 'macdh', 'kdjk', 'kdjd', 'kdjj']]
+    else:
+        df = ss[['open', 'close', 'high', 'low',
+                 'close_-1_d', 'close_5_sma', 'close_20_sma', 'close_60_sma', 'close_120_sma', 'close_200_sma',
+                 'close_20_sma_-1_d', 'close_60_sma_-1_d', 'close_120_sma_-1_d', 'close_5_ema',
+                 'close_20_ema', 'close_60_ema', 'close_120_ema', 'close_20_ema_-1_d', 'close_200_ema']]
     df['rate_sma20'] = df['close_20_sma'] / (df['close_20_sma'] - df['close_20_sma_-1_d'])
     df['rate_ema20'] = df['close_20_ema'] / (df['close_20_ema'] - df['close_20_ema_-1_d'])
     df['rate_sma60'] = df['close_60_sma'] / (df['close_60_sma'] - df['close_60_sma_-1_d'])
