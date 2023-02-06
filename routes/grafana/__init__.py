dacd2d799d2d0e5890077bd97cca6d31b3b9e10e
jxch <15553598935@163.com>
2023-02-06 16:31:48 +0800

    grafana api

--- routes/grafana/__init__.py
+++ routes/grafana/__init__.py
@@ -1,4 +1,3 @@
 from routes.grafana.gf_base_route import gf_base_route
-from routes.grafana.gf_bs_route import gf_bs_route
 
-grafana_routes = [gf_base_route, gf_bs_route]
+grafana_routes = [gf_base_route]
