3b555a38fb3da63a12cbfdf94261e9949da23a09
jxch <qianbuhan@proton.me>
2023-05-08 07:55:29 +0800

    更新版本

--- utils/proxy_utils.py
+++ utils/proxy_utils.py
@@ -5,5 +5,5 @@ import logging
 
 def proxy(proxy_host, proxy_port):
     logging.log(logging.INFO, f'开启网络代理: socks5 {proxy_host}:{proxy_port}')
-    socks.set_default_proxy(socks.SOCKS5, proxy_host, proxy_port)
+    socks.setdefaultproxy(socks.SOCKS5, proxy_host, proxy_port)
     socket.socket = socks.socksocket
